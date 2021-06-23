e9651e93a0481cebc4d14cfe1cc65de92b1d1707
hinkyma <osocki@gmail.com>
2021-06-23 07:23:37 +0300

    Create routes

--- routes/utils.py
+++ routes/utils.py
@@ -25,7 +25,8 @@ def get_graph(qs):
 
 def get_routes(request, form) -> dict:
     context = {'form': form}
-    qs = Train.objects.all()
+    qs = Train.objects.all().select_related('from_city', 'to_city')
+    # qs = Train.objects.all()
     graph = get_graph(qs)
     data = form.cleaned_data
     from_city = data['from_city']
@@ -48,4 +49,35 @@ def get_routes(request, form) -> dict:
             raise ValueError('Маршрут, через эти города невозможен')
     else:
         right_ways = all_ways
+    routes = []
+    all_trains = {}
+    for q in qs:
+        all_trains.setdefault((q.from_city_id, q.to_city_id), [])
+        all_trains[(q.from_city_id, q.to_city_id)].append(q)
+    for route in right_ways:
+        tmp = {}
+        tmp['trains'] = []
+        total_time = 0
+        for i in range(len(route) -1):
+            qs = all_trains[(route[i], route[i+1])]
+            q = qs[0]
+            total_time += q.travel_time
+            tmp['trains'].append(q)
+        tmp['total_time'] = total_time
+        if total_time <= travelling_time:
+            routes.append(tmp)
+    if not routes:
+        raise ValueError('Время в пути больше заданного')
+    sorted_routes = []
+    if len(routes) == 1:
+        sorted_routes = routes
+    else:
+        times = list(set(r['total_time'] for r in routes))
+        times = sorted(times)
+        for time in times:
+            for route in routes:
+                if time == route['total_time']:
+                    sorted_routes.append(route)
+    context['routes'] = sorted_routes
+    context['cities'] = {'from_city': from_city, 'to_city': to_city}
     return context
